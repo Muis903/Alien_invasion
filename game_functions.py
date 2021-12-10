a0c0d9da8acead873c2d61c32bf8685be42e0e77
Muis903 <mischaiv19@gmail.com>
2021-12-10 23:02:05 +0100

    Started with bullets.

--- game_functions.py
+++ game_functions.py
@@ -2,6 +2,25 @@ import sys
 
 import pygame
 
+
+def check_keydown_events(event, ship):
+    """Respond to keypresses."""
+    if event.key == pygame.K_RIGHT:
+        ship.moving_right = True
+            
+    elif event.key == pygame.K_LEFT:
+        ship.moving_left = True
+
+
+def check_keyup_events(event, ship):
+    """Respond to key releases."""
+    if event.key == pygame.K_RIGHT:
+        ship.moving_right = False
+            
+    elif event.key == pygame.K_LEFT:
+        ship.moving_left = False
+
+
 def check_events(ship):
     """Respond to keypresses and mouse events."""
     for event in pygame.event.get():
@@ -10,18 +29,10 @@ def check_events(ship):
             sys.exit()
     
         elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_RIGHT:
-                ship.moving_right = True
-            
-            elif event.key == pygame.K_LEFT:
-                ship.moving_left = True
-
+            check_keydown_event(event, ship)
+           
         elif event.type == pygame.KEYUP:
-            if event.key == pygame.K_RIGHT:
-                ship.moving_right = False
-            
-            elif event.key == pygame.K_LEFT:
-                ship.moving_left = False
+            check_keyup_events(event, ship)
 
         # Make the most recently drawn screen visible.
         pygame.display.flip()
@@ -35,4 +46,3 @@ def update_screen(ai_settings, screen, ship):
 
     # Make the most recently drawn screen visible.
     pygame.display.flip()
-
