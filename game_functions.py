4b8e8a87ca2641b5b028ead2d06fe9c40cd00d72
Muis903 <mischaiv19@gmail.com>
2021-12-15 20:51:29 +0100

    Created limiet of bullets at the same time.

--- game_functions.py
+++ game_functions.py
@@ -13,7 +13,11 @@ def check_keydown_events(event, ai_settings, screen, ship, bullets):
         ship.moving_left = True
 
     elif event.key == pygame.K_SPACE:
-        # Create a new bullet and add it to the bullets group.
+        fire_bullet(ai_settings, screen, ship, bullets)
+
+def fire_bullet(ai_settings, screen, ship, bullets):
+    # Fire a bullet if limiet not reached yet.
+    if len(bullets) < ai_settings.bullets_allowed:
         new_bullet = Bullet(ai_settings,screen, ship)
         bullets.add(new_bullet)
 
@@ -56,3 +60,16 @@ def update_screen(ai_settings, screen, ship, bullets):
 
     # Make the most recently drawn screen visible.
     pygame.display.flip()
+
+def update_bullets(bullets):
+        """Update position of bullets and get rid of old bullets."""
+        # Update bullets position
+
+        bullets.update()
+        
+        # Get rid of bullets thad have disappeared
+        for bullet in bullets.copy():
+            if bullet.rect.bottom <= 0:
+                bullets.remove(bullet)
+                print(len(bullets)) #For test
+   
