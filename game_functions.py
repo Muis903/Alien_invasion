fbbdc10e8df978e97875ed4c7cc66c1dd50aeecc
Muis903 <81823117+Muis903@users.noreply.github.com>
2022-02-01 23:21:02 +0100

    Ready to play

--- game_functions.py
+++ game_functions.py
@@ -3,9 +3,11 @@ from time import sleep
 import pygame
 from bullet import Bullet
 from alien import Alien
+import json
+
 
 def check_keydown_events(event, ai_settings, screen, stats,
-    ship, aliens, bullets):
+        ship, aliens, bullets):
     """Respond to keypresses."""
     if event.key == pygame.K_RIGHT:
         ship.moving_right = True
@@ -25,7 +27,7 @@ def check_keydown_events(event, ai_settings, screen, stats,
 
 
 def start_game(ai_settings, screen, stats, ship, aliens,
-    bullets):
+        bullets):
     if not stats.game_active:
         # Reset the game settings
         ai_settings.initialize_dynamic_settings()
@@ -60,12 +62,13 @@ def check_keyup_events(event, ship):
         ship.moving_left = False
 
 
-def check_events(ai_settings, screen, stats, play_button, ship, aliens, 
-        bullets):
+def check_events(ai_settings, screen, stats, sb, play_button, ship, 
+        aliens, bullets):
     """Respond to keypresses and mouse events."""
     for event in pygame.event.get():
 
         if event.type == pygame.QUIT:
+            save_high_score()
             sys.exit()
         
         elif event.type == pygame.KEYDOWN:
@@ -77,15 +80,15 @@ def check_events(ai_settings, screen, stats, play_button, ship, aliens,
 
         elif event.type == pygame.MOUSEBUTTONDOWN:
             mouse_x, mouse_y = pygame.mouse.get_pos()
-            check_play_button(ai_settings, screen, stats, play_button, ship,
+            check_play_button(ai_settings, screen, stats, sb, play_button, ship,
                 aliens, bullets, mouse_x, mouse_y)
 
         # Make the most recently drawn screen visible.
         pygame.display.flip()
 
 
-def check_play_button(ai_settings, screen, stats, play_button, ship, aliens,
-        bullets, mouse_x, mouse_y):
+def check_play_button(ai_settings, screen, stats, sb, play_button, ship, 
+        aliens, bullets, mouse_x, mouse_y):
     """Start a new game when the player clicks Play."""
     button_clicked = play_button.rect.collidepoint(mouse_x, mouse_y)
     if button_clicked and not stats.game_active:
@@ -94,9 +97,16 @@ def check_play_button(ai_settings, screen, stats, play_button, ship, aliens,
 
         # Hide the mouse cursor.
         pygame.mouse.set_visible(False)
+        
         # Reset the game statistics.
         stats.reset_stats()
         stats.game_active = True
+
+        # Reset the scoreboard images.
+        sb.prep_score()
+        sb.prep_high_score()
+        sb.prep_level()
+        sb.prep_ships()
 
         # Empty the list of aliens and bullets.
         aliens.empty()
@@ -154,11 +164,17 @@ def check_bullet_alien_collisions(ai_settings, screen, stats, sb, ship, aliens, 
         for aliens in collisions.values():
             stats.score += ai_settings.alien_points * len(aliens)
             sb.prep_score()
+        check_high_score(stats, sb)
     
     if len(aliens) == 0:
-        # Destroy existing bullets, speed up game, and create new fleet.
+        # If the entire fleet is destroyed, start a new level.
         bullets.empty()
         ai_settings.increase_speed()
+
+        # Increase level.
+        stats.level += 1
+        sb.prep_level()
+
         create_fleet(ai_settings, screen, ship, aliens)
 
 
@@ -203,11 +219,14 @@ def get_number_rows(ai_settings, ship_height, alien_height):
     return number_rows
 
 
-def ship_hit(ai_settings, stats, screen, ship, aliens, bullets):
+def ship_hit(ai_settings, screen, stats, sb, ship, aliens, bullets):
     """Respond to ship being hit by alien."""
     if stats.ships_left > 0:
         # Decrement ships left.
         stats.ships_left -= 1
+
+        # Update scoreboard.
+        sb.prep_ships()
 
         # Empty the list of aliens and bullets.
         aliens.empty()
@@ -224,17 +243,18 @@ def ship_hit(ai_settings, stats, screen, ship, aliens, bullets):
         stats.game_active = False
         pygame.mouse.set_visible(True)
 
-def check_aliens_bottom(ai_settings, stats, screen, ship, aliens, bullets):
+def check_aliens_bottom(ai_settings, screen, stats, sb, ship, aliens,
+        bullets):
     """Check if any aliens have reached the borrm of the screen."""
     screen_rect = screen.get_rect()
     for alien in aliens.sprites():
         if alien.rect.bottom >= screen_rect.bottom:
             # Treat this the same as if the ship got hit.
-            ship_hit(ai_settings, stats, screen, ship, aliens, bullets)
+            ship_hit(ai_settings, screen, stats, sb, ship, aliens, bullets)
             break
 
 
-def update_aliens(ai_settings, stats, screen, ship, aliens, bullets):
+def update_aliens(ai_settings, screen, stats, sb, ship, aliens, bullets):
     """
     Check if the fleet is at an edge, ,
         and then update the position of all aliens in the fleet.
@@ -244,10 +264,10 @@ def update_aliens(ai_settings, stats, screen, ship, aliens, bullets):
 
     # Look for alien-ship collisions.
     if pygame.sprite.spritecollideany(ship, aliens):
-        ship_hit(ai_settings, stats, screen, ship, aliens, bullets)
+        ship_hit(ai_settings, screen, stats, sb, ship, aliens, bullets)
 
     # Look for aliens hitting the bottom of the screen.
-    check_aliens_bottom(ai_settings, stats, screen, ship, aliens, bullets)
+    check_aliens_bottom(ai_settings, screen, stats, sb, ship, aliens, bullets)
 
 
 def check_fleet_edges(ai_settings, aliens):
@@ -263,3 +283,18 @@ def change_fleet_direction(ai_settings, aliens):
     for alien in aliens.sprites():
         alien.rect.y += ai_settings.fleet_drop_speed
     ai_settings.fleet_direction *= -1
+
+
+def check_high_score(stats, sb):
+    """Check to see if there's a new high score."""
+    if stats.score > stats.high_score:
+        stats.high_score = stats.score
+        sb.prep_high_score()
+
+
+def save_high_score(stats):
+    return
+   # """Save the high score in archive."""
+   # #d = {"Name": input("Name: "), "Score": str(stats.high_score)}
+   # with open archive
+
